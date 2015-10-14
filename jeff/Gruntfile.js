c9c5daec86a49a94b0799eaacb5a615ef1a2f438
Jeff Reiher <jeffreiher@gmail.com>
2015-10-14 16:16:38 -0400

    set up grunt work flow

---
+++ jeff/Gruntfile.js
@@ -0,0 +1,56 @@
+// Gruntfile.js
+module.exports = function(grunt) {
+  grunt.initConfig({
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+      // when this task is run, lint the Gruntfile and all js files in src
+      build: ['Grunfile.js', 'static/**/*.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          //concats and minify's
+          'static/dist/js/main.min.js': ['static/js/main.js']
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'static/dist/css/styles.min.css': ['static/css/styles.css']
+        }
+      }
+    },
+    watch: {
+      files: ['static/css/*.css', 'static/js/*.js'],
+      tasks: ['default']
+    }
+
+
+  });
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+};
