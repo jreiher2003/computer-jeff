4bdfcc56f4f2c85a52975c86fbfdb658a797a4b3
Jeff Reiher <jeffreiher@gmail.com>
2015-10-15 18:09:02 -0400

    added url routes to app

--- jeff/views.py
+++ jeff/views.py
@@ -4,4 +4,18 @@ from jeff import app
 @app.route('/')
 def index():
     return render_template('base.html')
+
+@app.route('/virus-removal')
+def virusremoval():
+	return render_template('virusremoval.html')
+
+@app.route('/computer-repair')
+def computerrepair():
+	return render_template('computerrepair.html')
+
+@app.route('/contact-us')
+def contact():
+	return render_template('contact.html')
+
+
     
