48d43c61dae89a777afdbc3e082c901875e7b343
Jeff Reiher <jeffreiher@gmail.com>
2015-11-02 10:31:33 -0500

    add page urls to project

--- jeff/views.py
+++ jeff/views.py
@@ -9,10 +9,14 @@ def index():
 def virusremoval():
 	return render_template('virus-removal.html')
 
-@app.route('/computer-repair')
+@app.route('/computer-tune-up')
 def computerrepair():
 	return render_template('computer-tune-up.html')
 
+@app.route('/pricing')
+def pricing():
+	return render_template('pricing.html')
+	
 @app.route('/contact-us')
 def contact():
 	return render_template('contact.html')
