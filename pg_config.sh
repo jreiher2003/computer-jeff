59e052cb3b52f3146a8f94c8efc9b9aecd76ec64
Jeff Reiher <jreiher2003@yahoo.com>
2016-07-10 21:15:39 +0000

    Create requirements.txt and add provisions to pg_config.sh

--- pg_config.sh
+++ pg_config.sh
@@ -4,3 +4,11 @@ sudo apt-get install -y upgrade
 sudo apt-get install -y python-dev 
 sudo apt-get install -y python-pip
 sudo apt-get install -y git 
+
+sudo pip install virtualenvwrapper 
+export WORKON_HOME=~/Envs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv computer-jeff 
+
+pip install Flask 
